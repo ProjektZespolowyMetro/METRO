c2b627df8c969dba92466e4584800fb218c846f2
ABardWithAWard <mateuszwatly@gmail.com>
2025-11-09 17:43:41 +0100

    Added simple map rendering and road drawing

--- frontend/src/screens/HelloWorldScreen.tsx
+++ frontend/src/screens/HelloWorldScreen.tsx
@@ -1,28 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-const HelloWorldScreen: React.FC = () => {
-  const [message, setMessage] = useState<string>("Loading...");
+export default function RoadsMap() {
+  const mapRef = useRef<L.Map | null>(null);
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/hello-world/")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.text();
-      })
-      .then((data) => setMessage(data))
-      .catch((error) => {
-        console.error("Error fetching message:", error);
-        setMessage("Error loading message");
-      });
+    if (!mapRef.current && mapContainerRef.current) {
+      // init and center on cracow
+      mapRef.current = L.map(mapContainerRef.current).setView([50.0647, 19.945], 13);
+
+      // openstreetmap tiles, generates background
+      // if deleted we will have only red lines from geojson
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "© OpenStreetMap contributors",
+      }).addTo(mapRef.current);
+
+      // roads from geojson
+      fetch("cracow-streets.geojson")
+        .then((res) => res.json())
+        .then((geojson) => {
+          const roadsLayer = L.geoJSON(geojson, {
+            style: { color: "red", weight: 2 },
+          }).addTo(mapRef.current!);
+
+          mapRef.current!.fitBounds(roadsLayer.getBounds());
+        })
+        .catch((err) => console.error("Failed to load GeoJSON:", err));
+    }
   }, []);
 
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>{message}</h1>
-    </div>
-  );
-};
-
-export default HelloWorldScreen;
+  return <div ref={mapContainerRef} style={{ width: "100%", height: "100vh" }} />;
+}
