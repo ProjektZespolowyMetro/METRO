6846dd2084e4833a36447c04986910badda1bffb
Mikolaj Mroczek <mikolaj.mroczek.dev@gmail.com>
2026-01-19 00:04:54 +0100

    added bus/tram station location data. Added modular classes to handle loading and searching location points

---
+++ backend/core/SearchStrategy.py
@@ -0,0 +1,45 @@
+import abc
+import json
+import math
+
+class SearchStrategy(abc.ABC):
+    @abc.abstractmethod
+    def prepare_data(self, stations_list: list):
+        """
+        Tutaj algorytm przygotowuje dane pod siebie.
+        BruteForce tylko je zapisze.
+        KDTree tutaj zbuduje drzewo.
+        """
+        pass
+
+    @abc.abstractmethod
+    def find_nearest(self, lat, lon):
+        """
+        Zwraca (najblizszy_przystanek, dystans)
+        """
+        pass
+
+
+class BruteForceSearch(SearchStrategy):
+
+    def __init__(self):
+        self.stations = []
+
+    def prepare_data(self, stations_list: list):
+        self.stations = stations_list
+
+    def find_nearest(self, lat, lon):
+        best_station = None
+        min_dist = float('inf')
+
+        for station in self.stations:
+            s_lat = station["coords"]["lat"]
+            s_lon = station["coords"]["lon"]
+            
+            curr_dist = math.sqrt((s_lat - lat)**2 + (s_lon - lon)**2)
+
+            if curr_dist < min_dist:
+                min_dist = curr_dist
+                best_station = station
+        
+        return best_station, min_dist
