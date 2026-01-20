5abd26f0a0b8899a7ad73630a215b09907b20a0d
Mateusz Wątły <95935287+ABardWithAWard@users.noreply.github.com>
2026-01-21 00:10:09 +0100

    Added comments

--- frontend/src/contexts/PinsContext.tsx
+++ frontend/src/contexts/PinsContext.tsx
@@ -1,16 +1,17 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
 
 export type Pin = {
-    id: string;
-    lat: number;
-    lng: number;
-    number?: number;
-    name?: string;
+    id: string; //used to identify unset pins
+    lat: number; //latitude 
+    lng: number; //longitude 
+    number?: number; // order of stations
+    name?: string; // both are null when adding empty pins,
+    // name is unused for now
 };
 
 type PinsContextType = {
     pins: Pin[];
-    setPins: React.Dispatch<React.SetStateAction<Pin[]>>;
+    setPins: React.Dispatch<React.SetStateAction<Pin[]>>; //type of useState
     addPin: (pin: Pin) => void;
     updatePin: (updatedPin: Pin) => void;
     removePin: (id: string) => void;
@@ -19,12 +20,14 @@ type PinsContextType = {
 
 export const usePins = () => {
     const context = useContext(PinsContext);
+    // In case of usage outside this provider throw an error
     if (!context) {
         throw new Error('usePins must be used within PinsProvider');
     }
     return context;
 };
 
+// Undefined outside this provider, default value is undefined as well.
 const PinsContext = createContext<PinsContextType | undefined>(undefined);
 
 export const PinsProvider: React.FC<{ children: React.ReactNode }> = ({
@@ -34,19 +37,24 @@ export const PinsProvider: React.FC<{ children: React.ReactNode }> = ({
         // load from localStorage on init
         try {
             const saved = localStorage.getItem('pins');
+            //if default value (undefined) then we return an empty array.
+            //if found anything then save whatever found
             return saved ? JSON.parse(saved) : [];
         } catch {
             return [];
         }
     });
 
-    // persist pins to localStorage on change
+    // save on first render and whenever array of pins is altered
     useEffect(() => {
         localStorage.setItem('pins', JSON.stringify(pins));
     }, [pins]);
 
+    //Copy all existing pins into a new array and append a new pin
     const addPin = (pin: Pin) => setPins((prev) => [...prev, pin]);
 
+    // Instead of removing and adding pin with passed id we loop
+    // through the array to avoid copying it twice
     const updatePin = (updatedPin: Pin) =>
         setPins((prev) =>
             prev.map((p) => (p.id === updatedPin.id ? updatedPin : p))
@@ -61,6 +69,7 @@ export const PinsProvider: React.FC<{ children: React.ReactNode }> = ({
         }
     };
 
+    // Every nested component in App.tsx has access to pin logic
     return (
         <PinsContext.Provider
             value={{ pins, setPins, addPin, updatePin, removePin, clearPins }}
